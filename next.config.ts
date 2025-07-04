a61ee7b540e50897038e51907de4cdfba13a9637
bee bees <ahmedyoussefabbes@gmail.com>
2025-07-04 13:49:11 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'api.dicebear.com',
         port: '',
-        pathname: '/**',
+        pathname: '/8.x/avataaars/svg',
       },
       {
         protocol: 'https',
